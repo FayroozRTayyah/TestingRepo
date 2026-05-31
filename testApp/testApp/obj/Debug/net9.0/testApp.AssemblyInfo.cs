033092cf9dc3eb60b6f7d838d0e67632b40e11d9
frtayyah <municipaltynablus@gmail.com>
2026-05-31 10:00:43 +0200

    ttt

--- testApp/testApp/obj/Debug/net9.0/testApp.AssemblyInfo.cs
+++ testApp/testApp/obj/Debug/net9.0/testApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("testApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a483698e48f2323e8fd7cc5556b03917cde76975")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ab590de7ec852da50a4349b4d6d0a090db81102")]
 [assembly: System.Reflection.AssemblyProductAttribute("testApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("testApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
